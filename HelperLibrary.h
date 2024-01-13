dca581c340c62e7dcce7580d79387023054764c0
faxcorp <37246339+faxcorp@users.noreply.github.com>
2024-01-14 01:26:01 +0200

    Update HelperLibrary.h

--- HelperLibrary.h
+++ HelperLibrary.h
@@ -1,27 +1,28 @@
-// 2017 WindyDarian (RuoyuF). MIT License
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
-
+#include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "HelperLibrary.generated.h"
+#include "GMCPPBlueprintFunctionLibrary.generated.h"
+
 
 /**
  *
  */
 UCLASS()
-class WILDHUNT_API UHelperLibrary : public UBlueprintFunctionLibrary
+class GM_API UGMCPPBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "HelperLibrary")
-	static FVector CalculateProjectileDirection(FVector Target, FVector Origin, FVector Gravity, float Speed, bool& bWillHit, float& Time);
+		UFUNCTION(BlueprintCallable, BlueprintPure, Category = "HelperLibrary")
+		static FVector CalculateProjectileDirection(FVector Target, FVector Origin, FVector Gravity, float Speed, bool& bWillHit, float& Time);
 
 
 	/**
 	* Calculate a projectile direction to hit a target with constant velocity
 	*/
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "HelperLibrary")
-	static FVector CalculateProjectileDirectionForMovingTarget(FVector Target, FVector TargetVelocity, FVector Origin, FVector Gravity, float Speed, bool& bWillHit, float& Time, int Iterations = 5, float EpsilonTime = 0.01f);
+		static FVector CalculateProjectileDirectionForMovingTarget(FVector Target, FVector TargetVelocity, FVector Origin, FVector Gravity, float Speed, bool& bWillHit, float& Time, int Iterations = 5, float EpsilonTime = 0.01f);
 
 };
